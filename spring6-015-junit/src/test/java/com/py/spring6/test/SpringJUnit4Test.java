d19693791fb5b361de15c31c7f9134b7fe3afbba
pengyang <20844@qq.com>
2023-11-05 07:49:57 +0800

    Spring整合JUnit4 & 5

---
+++ spring6-015-junit/src/test/java/com/py/spring6/test/SpringJUnit4Test.java
@@ -0,0 +1,44 @@
+package com.py.spring6.test;
+
+import com.py.spring6.bean.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author PengYang
+ * @version 1.0
+ * @className SpringJUnit4Test
+ * @since 1.0
+ **/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring.xml")
+public class SpringJUnit4Test {
+
+    @Autowired
+    private User user;
+
+    @Test
+    public void testUser(){
+        /*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User user = applicationContext.getBean("user", User.class);*/
+        System.out.println(user.getName());
+    }
+
+    @Test
+    public void testUser2(){
+        /*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User user = applicationContext.getBean("user", User.class);*/
+        System.out.println(user.getName());
+    }
+
+    @Test
+    public void testUser3(){
+        /*ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        User user = applicationContext.getBean("user", User.class);*/
+        System.out.println(user.getName());
+    }
+
+}
---
+++ spring6-015-junit/src/test/java/com/py/spring6/test/SpringJUnit5Test.java
@@ -0,0 +1,27 @@
+package com.py.spring6.test;
+
+import com.py.spring6.bean.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+/**
+ * @author PengYang
+ * @version 1.0
+ * @className SpringJUnit5Test
+ * @since 1.0
+ **/
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("classpath:spring.xml")
+public class SpringJUnit5Test {
+
+    @Autowired
+    private User user;
+
+    @Test
+    public void testUser(){
+        System.out.println(user.getName());
+    }
+}
